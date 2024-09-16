54e3c931517fbb87a9e9b9f44d76041109964c84
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2024-09-16 14:22:12 -0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,9 @@
+public class Account{
+    String username;
+    String password;
+    public Account(String username, String password){
+        this.username = username;
+        this.password = password;
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main{
+
+    static boolean check(Account account){
+        //accepts type Account
+        //checks if Account name and password combo exists(basically if Account in parameter is already a valid object in addAccounts list)
+        //returns true or false
+        return true;
+    }
+    
+    static void addAccount(Account account){
+        //accepts type Account
+        //makes a list of valid accounts of type "Account" (probably arraylist)
+        //
+    }
+
+    public static void main(String[] args){
+        String userName, userPassword;
+        Scanner scanner = new Scanner(System.in);
+        
+        // account enteredAccountInfo;
+        // userName = scanner.next(); //can be used later to allow for new Account
+        // userPassword = scanner.next();
+        
+        //enteredAccountInfo = new Account(userName, userPassword);
+        //write a line that takes in the user input to continually reinitialize an Account to continue using addAccount method
+    }
+}
