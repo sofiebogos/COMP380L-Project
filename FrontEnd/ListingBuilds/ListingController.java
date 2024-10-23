563888b321da03e193b12cdece8b011f0b410407
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2024-10-23 13:59:07 -0700

    Add files via upload

---
+++ FrontEnd/ListingBuilds/ListingController.java
@@ -0,0 +1,37 @@
+package hellofx;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ListingController {
+    
+    @FXML
+    ImageView carPic;
+    Button nextCar;
+    Button previuosCar;
+
+    int i=0;
+
+    public void nextImage(){
+        if (i >= Main.mcQueenPics.size() -1){
+            i = 0;
+        }
+        else {
+            i++;
+        }
+            carPic.setImage(Main.mcQueenPics.get(i));
+    }
+
+    public void previousImage(){
+        if (i == 0){
+            i = Main.mcQueenPics.size() -1;
+        }
+        else {
+            i--;
+        }
+            carPic.setImage(Main.mcQueenPics.get(i));
+    }
+}
