7fcb3005482f142f8a58291ee410bfc660e19ca0
allisonvan <allison.vanegas.607@my.csun.edu>
2024-10-28 13:46:15 -0700

    Update controller.java

--- FrontEnd/controller.java
+++ FrontEnd/controller.java
@@ -19,10 +19,55 @@ public class controller {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    private TextField createFirst;
+    @FXML
+    private TextField createLast; 
+    @FXML
+    private TextField createEmail; 
+    @FXML
+    private TextField createPassword;
+    @FXML
+    private TextField createMonthDOB;
+    @FXML
+    private TextField createDayDOB;
+    @FXML
+    private TextField createYearDOB;
+    @FXML
+    private TextField createAddress;
+    @FXML
+    private TextField createCity;
+    @FXML
+    private TextField createState;
+    @FXML
+    private TextField createZIP;
+    @FXML
+    private TextField createCardnum;
+    @FXML
+    private TextField createCVC;
+    @FXML
+    private TextField createExp;
+
     public void switchToLogin(ActionEvent event) throws IOException{
         root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+
+        String firstName = createFirst.getText();
+        String lastName = createLast.getText();
+        String email2 = createEmail.getText();
+        String password2 = createPassword.getText();
+        String DOBmonth = createMonthDOB.getText();
+        String DOBday = createDayDOB.getText();
+        String DOByear = createYearDOB.getText();
+        String address = createAddress.getText();
+        String city = createCity.getText();
+        String state = createState.getText();
+        String ZIP = createZIP.getText();
+        String cardNumber = createCardnum.getText();
+        String CVC = createCVC.getText(); 
+        String expDate = createExp.getText();
+        System.out.println("Create Account info:"+email2+ password2+ DOBmonth + DOBday +DOByear + address + city + state + ZIP + cardNumber + CVC + expDate);
         stage.setScene(scene);
         stage.show();
     }
@@ -49,7 +94,7 @@ public class controller {
 
         System.out.println("You have successfully logged in!"); 
         System.out.println("Email: " + email);
-        System.out.println("Password: " + password);
+        System.out.println("Password: " + password + email);
     }
 
     
