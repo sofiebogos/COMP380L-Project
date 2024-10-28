1903ba2f48738fd0ea8f6449a71872c382c2fe31
allisonvan <allison.vanegas.607@my.csun.edu>
2024-10-28 10:07:38 -0700

    Updated controller.java

--- FrontEnd/controller.java
+++ FrontEnd/controller.java
@@ -1,27 +1,57 @@
 package hellofx;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 
 public class controller {
-     @FXML
-    private TextField textfield1;
+    private Stage stage; 
+    private Scene scene;
+    private Parent root;
+
+    public void switchToLogin(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToCreateAcc(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("createaccount.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
 
     @FXML
-    private TextField textfield2;
+    private TextField emailtext;
 
+    @FXML
+    private TextField passwordtext;
+ 
     @FXML
     public void Enter(ActionEvent e) {
-        String username = textfield1.getText();
-        String password = textfield2.getText();
+        String email = emailtext.getText();
+        String password = passwordtext.getText();
 
         System.out.println("You have successfully logged in!"); 
-        System.out.println("Username: " + username);
+        System.out.println("Username: " + email);
         System.out.println("Password: " + password);
     }
 
-
+    
 
 }
