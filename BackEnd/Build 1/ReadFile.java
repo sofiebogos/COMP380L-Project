c0c876bb88436dd4cc95aff82498c5edfe369a9e
Max-Woodruff <richard.woodruff.951@my.csun.edu>
2024-10-30 14:56:01 -0700

    Update ReadFile.java
    Readfile now takes a filename, instead of being hard coded

--- BackEnd/Build 1/ReadFile.java
+++ BackEnd/Build 1/ReadFile.java
@@ -6,17 +6,29 @@ import java.util.Objects;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
-    public static void main(String[] args) { // This function can only read listings, but should serve as a template
+    public static void main(String[] args) {
+        ReadListing("listing.txt");
+    }
+
+    // to do: this function should only take a file name and call a function
+    // specific to that file
+
+    public static void ReadListing(String filename) { // This function can only read listings, but should serve as a
+                                                      // template
+
         List<Listing> listings = new ArrayList<>();
         try {
-            File myObj = new File("C:\\Users\\Max\\Desktop\\Fall 24\\COMP 380\\class\\listing.txt");
-            Scanner myReader = new Scanner(myObj);
+            File file = new File(filename);
+            Scanner myReader = new Scanner(file);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
-                String[] parts = data.split(";");
-                listings.add(new Listing(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7],
-                        parts[8], parts[9], parts[10], parts[11]));
+                if (data != "") {
+                    System.out.println(data);
+                    String[] parts = data.split(";");
+                    listings.add(
+                            new Listing(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7],
+                                    parts[8], parts[9], parts[10], parts[11]));
+                }
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -25,10 +37,10 @@ public class ReadFile {
         }
     }
 
-    public static boolean Compare(String toCompare) {
+    public static boolean Compare(String toCompare, String filename) {
         try {
-            File myObj = new File("C:\\Users\\Max\\Desktop\\Fall 24\\COMP 380\\class\\listing.txt");
-            Scanner myReader = new Scanner(myObj);
+            File file = new File(filename);
+            Scanner myReader = new Scanner(file);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 if ((Objects.equals(data, toCompare)) == true) {
