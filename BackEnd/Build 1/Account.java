a79c6c22493100dd105777b22e6672c1860e1421
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2024-11-10 17:13:55 -0800

    Update Account.java

--- BackEnd/Build 1/Account.java
+++ BackEnd/Build 1/Account.java
@@ -1,9 +1,9 @@
 public class Account{
     private String firstName, lastName, email, password;
-    private int[3] DOB;
+    private int[] DOB = new int[3];
     private String city, state, street;
     private int zipcode;
-    public Account(String firstName, String lastName; String email, String password, int[3] DOB, String city, String state, String street){
+    public Account(String firstName, String lastName, String email, String password, int[] DOB, String city, String state, String street){
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
