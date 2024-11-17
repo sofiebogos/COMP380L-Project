795f874d663d596eb9a0df7dc8c981a6b639ff60
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2024-11-16 20:36:34 -0800

    update

--- FrontEnd/ListingBuilds/src/hellofx/Main.java
+++ FrontEnd/ListingBuilds/src/hellofx/Main.java
@@ -95,7 +95,7 @@ public class Main extends Application {
 		myStage.getIcons().add(icon);
 		myStage.setTitle("Jams Auto Sales");
 
-		Parent root = FXMLLoader.load(getClass().getResource("posting.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("listing.fxml"));
 
 		Scene scene = new Scene(root, Color.GRAY);
 		myStage.setScene(scene);
