802b9a0aa2880b0012f6c3be990a3099508371b5
allisonvan <allison.vanegas.607@my.csun.edu>
2024-11-25 13:08:55 -0800

    Update Account.java

--- BackEnd/Build 1/Account.java
+++ BackEnd/Build 1/Account.java
@@ -2,21 +2,23 @@ import java.util.ArrayList;
 
 public class Account{
     private String firstName, lastName, email, password;
-    private String[] DOB = new String[3];
+    private String month, day, year; 
     private String city, state, address, EXPDate, zipcode;
-    private int cardNumber, CVC;
+    private String cardNumber, CVC;
     public int listingCount = 0;
     private ArrayList<Integer> listings = new ArrayList<>();
 
     public Account(String firstName, String lastName, String email, String password, 
-                   String[] DOB, String address, String city, String state, 
-                   String zipcode, int cardNumber, int CVC, String EXPDate           ){
+                   String month, String day, String year, String address, String city, String state, 
+                   String zipcode, String cardNumber, String CVC, String EXPDate){
 
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.DOB = DOB;
+        this.month = month;
+        this.day = day;
+        this.year = year;
         this.address = address;
         this.city = city;
         this.state = state;
