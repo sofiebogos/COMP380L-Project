3c12ea9f443684a39584751fad32810080e98734
Max-Woodruff <richard.woodruff.951@my.csun.edu>
2024-11-25 13:20:42 -0800

    Added Javadoc comments, made the lists public

--- BackEnd/Build 1/ReadFile.java
+++ BackEnd/Build 1/ReadFile.java
@@ -5,17 +5,48 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Scanner; // Import the Scanner class to read text files
 
+/**
+ * @author @version
+ *         The interface to the text files that store data
+ *         any time something needs to be read from a file this class is called
+ */
+
 public class ReadFile {
+    public static List<car> carList = new ArrayList<>();
+    public static List<Listing> listingList = new ArrayList<>();
+    public static List<Account> accoubtList = new ArrayList<>();
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        ReadListing("listing.txt");
+        Read("listing.txt");
     }
 
-    // to do: this function should only take a file name and call a function
-    // specific to that file
-    
-    public static void ReadListing(String filename) { // This function can only read listings, but should serve as a
-                                                      // template
-        List<Listing> listings = new ArrayList<>();
+    /**
+     * Read gets passed a @param filename which it uses to call the respective
+     * function to read the file
+     * each file needs its own function because they have different array lengths
+     */
+
+    public static void Read(String filename) {
+        if (filename == "car.txt") {
+            ReadCar();
+        }
+        if (filename == "listing.txt") {
+            ReadListing();
+        }
+
+    }
+
+    /**
+     * Reads the car.txt file
+     * Splits it into an array, then passes the array to the constructor
+     */
+
+    public static void ReadCar() {
+        String filename = "car.txt";
+
         try {
             File file = new File(filename);
             Scanner myReader = new Scanner(file);
@@ -24,9 +55,8 @@ public class ReadFile {
                 if (data != "") {
                     System.out.println(data);
                     String[] parts = data.split(";");
-                    listings.add(
-                            new Listing(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7],
-                                    parts[8], parts[9], parts[10], parts[11]));
+                    carList.add(new car(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6],
+                            parts[7], parts[8], parts[9]));
                 }
             }
             myReader.close();
@@ -34,10 +64,15 @@ public class ReadFile {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
     }
 
-    public static void ReadAccount(String filename){
-        List<Account> accounts = new ArrayList<>();
+    /**
+     * Reads the account.txt file
+     * Splits it into an array, then passes the array to the constructor
+     */
+
+    public static void ReadAccount(String filename) {
         try {
             File file = new File(filename);
             Scanner myReader = new Scanner(file);
@@ -46,7 +81,7 @@ public class ReadFile {
                 if (data != "") {
                     System.out.println(data);
                     String[] parts = data.split(";");
-                    accounts.add(
+                    accoubtList.add(
                             new Account(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7],
                                     parts[8], parts[9], parts[10], parts[11], parts[12], parts[13]));
                 }
@@ -57,6 +92,42 @@ public class ReadFile {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Reads the listing.txt file
+     * Splits it into an array, then passes the array to the constructor
+     */
+
+    public static void ReadListing() { // This function can only read listings, but should serve as a template
+        String filename = "listing.txt";
+        try {
+            File file = new File(filename);
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (data != "") {
+                    System.out.println(data);
+                    String[] parts = data.split(";");
+                    listingList.add(new Listing(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6],
+                            parts[7], parts[8], parts[9], parts[10], parts[11]));
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Compare takes in two strings
+     * 
+     * @param toCompare the string to look for
+     * @param filename  name of the file to search through
+     *                  goes through the file looking to see if it the string is
+     *                  inside it
+     * @return true if string is in file, false if not
+     */
 
     public static boolean Compare(String toCompare, String filename) {
         try {
@@ -76,5 +147,34 @@ public class ReadFile {
             e.printStackTrace();
         }
         return false;
+    }
+
+    /**
+     * @param toCompare takes in a string to compare, @param filename takes in the
+     *                  name of the file
+     *                  goes through the file looking to see if a line has part of a
+     *                  string inside it, if it is then return the whole line
+     * @return 0 if not found, @return the string if it is
+     */
+
+    public static String FindAndReturn(String toCompare, String filename) { // ONLY CHECKS FOR A PARTIAL. IF HANDED BAD
+                                                                            // DATA WILL RETURN BAD RESULTS
+        try {
+            File file = new File(filename);
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if ((toCompare.toLowerCase().contains(data)) == true) {
+                    myReader.close();
+                    return toCompare;
+                }
+            }
+            myReader.close();
+            return "0";
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return "0";
     }
 }
