ed65a4bede22d337881fa389126c136d8ed7876b
Sofie Bogosyan <159590095+sofiebogos@users.noreply.github.com>
2024-11-25 13:53:31 -0800

    Update ReadFileTest.java

--- BackEnd/Test/ReadFileTest.java
+++ BackEnd/Test/ReadFileTest.java
@@ -1,1 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
 
+class ReadFileTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readCar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void readListing() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compare() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findAndReturn() {
+    }
+}
