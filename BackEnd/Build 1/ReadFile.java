32ab4a9c69e08822df89161c8221e8090c1424d5
allisonvan <allison.vanegas.607@my.csun.edu>
2024-11-25 13:11:58 -0800

    Add ReadAccount to ReadFile.java

--- BackEnd/Build 1/ReadFile.java
+++ BackEnd/Build 1/ReadFile.java
@@ -12,10 +12,9 @@ public class ReadFile {
 
     // to do: this function should only take a file name and call a function
     // specific to that file
-
+    
     public static void ReadListing(String filename) { // This function can only read listings, but should serve as a
                                                       // template
-
         List<Listing> listings = new ArrayList<>();
         try {
             File file = new File(filename);
@@ -28,6 +27,28 @@ public class ReadFile {
                     listings.add(
                             new Listing(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7],
                                     parts[8], parts[9], parts[10], parts[11]));
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void ReadAccount(String filename){
+        List<Account> accounts = new ArrayList<>();
+        try {
+            File file = new File(filename);
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (data != "") {
+                    System.out.println(data);
+                    String[] parts = data.split(";");
+                    accounts.add(
+                            new Account(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7],
+                                    parts[8], parts[9], parts[10], parts[11], parts[12], parts[13]));
                 }
             }
             myReader.close();
